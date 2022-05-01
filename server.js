0c148d82aab93e0588196e55723168f8c539ceea
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2022-05-01 15:19:37 +0530

    Auth services created

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
 import connectDatabase from "./database/connection.js"
+import { login, renewAccessToken } from "./services/authenticationService.js";
+import { deleteUser, findUsers, registerUser, updateUser } from "./services/userService.js";
 
 // Enable .env file
 dotenv.config();
@@ -15,6 +17,15 @@ const app = express();
 app.use(cors({origin: '*'}));
 // Let express know, to use Json for http requests and response.
 app.use(express.json());
+
+app.post('/users/login', login);
+app.get('users/refreshtoken', renewAccessToken);
+
+app.get('/users', findUsers);
+app.post('/users', registerUser);
+app.put('/users', updateUser);
+app.delete('/users', deleteUser);
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ services/authenticationService.js
@@ -0,0 +1,37 @@
+
+import user from "../model/user.js";
+import jwt from "jsonwebtoken";
+
+const login = (req, res) => {
+    const { email, password } = req.body;
+    const filter = { email, password };
+
+    user.find(filter, (error, users) => {
+        if (error) {
+            return res.status(500)
+                .json(error);
+
+        } else if (users.length > 0){
+            const { id, role } = users[0];
+            const authBody = { id, role };
+            const accessToken = jwt.sign(authBody, process.env.ACCESS_SECRET, { expiresIn: '1h' });
+            const refreshToken = jwt.sign(authBody, process.env.REFRESH_SECRET);
+            res.status(200).json({accessToken: accessToken, refreshToken: refreshToken});
+        } else {
+            res.status(401).json('invalid credentials');
+        }
+    });
+}
+
+const renewAccessToken = (req, res) => {
+    const { refreshToken } = req.headers;
+    if(!refreshToken) return res.status(401).json('No refresh token!');
+
+    jwt.verify(refreshToken, process.env.REFRESH_SECRET, (error, value) => {
+        if (error) return res.status(401).json('Invalid refresh token!');
+        const accessToken = jwt.sign(authBody, process.env.ACCESS_SECRET, { expiresIn: '1h' });
+        res.status(200).json({accessToken: accessToken});
+    });
+}
+
+export { login, renewAccessToken };
