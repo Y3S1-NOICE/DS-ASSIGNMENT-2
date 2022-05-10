4899ae6d7074bb2f43063067e1eef067150a35ec
Shehan Bartholomeusz <it20274702@my.sliit.lk>
2022-05-10 12:06:51 +0530

    Nav bar

--- rsp-frontend/src/index.js
+++ rsp-frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import NavBar from './components/Navbar';
+import Typography from '@mui/material/Typography';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Typography>
+      <NavBar />
+      <App />
+    </Typography>
   </React.StrictMode>
 );
 
