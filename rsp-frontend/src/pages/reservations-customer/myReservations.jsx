833abffa214cecf3bd36ab3c8f52079831ce6cdb
amanthajayathilake <amanthajayathilake@gmail.com>
2022-05-16 03:17:53 +0530

    my reservations changes

--- rsp-frontend/src/pages/reservations-customer/myReservations.jsx
+++ rsp-frontend/src/pages/reservations-customer/myReservations.jsx
@@ -8,7 +8,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import { fetchUsers } from '../../api/userServiceApi';
 import jwtDecode from 'jwt-decode';
-import { fetchReservation, deleteReservation } from '../../api/reservationCustomerApi';
+import { getAuth } from '../../util/Utils';
+import { fetchReservation, deleteReservation, fetchAllReservations } from '../../api/reservationCustomerApi';
 import toast, { Toaster } from 'react-hot-toast';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Stack from '@mui/material/Stack';
@@ -18,14 +19,15 @@ import IconButton from '@mui/material/IconButton';
 import EditMyReservation from '../../components/reservations/EditCustomerReservationDialog';
 
 const MyReservations = () => {
-    const userId = jwtDecode(localStorage.getItem('authentication')).id;
+    const loggedUserId = getAuth().id
     const [reservation, setReservation] = useState([]);
+    const [reservations, setReservations] = useState([]);
     const [user, setUser] = useState();
     const [editOpen, setEditOpen] = useState(false);
 
     useEffect(() =>{
         function getUser() {
-            fetchUsers(`?id=${userId}`)
+            fetchUsers(`?id=${loggedUserId}`)
             .then((res) =>{
                 setUser(res.data);
                 console.log(res.data);
@@ -41,8 +43,8 @@ const MyReservations = () => {
     }, []);
 
     const handleGetReservations = () => {
-        fetchReservation(userId).then(res => {
-            setReservation(res.data);
+        fetchAllReservations().then(res => {
+            setReservations(res.data);
             console.log(res.data);
         }).catch(() => {
             toast.error('Error!', {
@@ -59,6 +61,26 @@ const MyReservations = () => {
             });
         });
     }
+
+    // const handleGetReservations = () => {
+    //     fetchReservation(userId).then(res => {
+    //         setReservation(res.data);
+    //         console.log(res.data);
+    //     }).catch(() => {
+    //         toast.error('Error!', {
+    //             position: "top-right",
+    //             style: {
+    //               padding: '16px',
+    //               color: 'white',
+    //               background: '#FF0000'
+    //             },
+    //             iconTheme: {
+    //               primary: 'red',
+    //               secondary: '#FFFAEE',
+    //             },
+    //         });
+    //     });
+    // }
 
       const handleDeleteReservation = (id) => {
         deleteReservation(id)
@@ -111,23 +133,26 @@ const MyReservations = () => {
                     <TableHead>
                         <TableRow>
                             <TableCell style={{fontWeight:"700"}}>Hotel Name</TableCell>
-                            <TableCell align="left" style={{fontWeight:"700"}}>Reservee Name</TableCell>
-                            <TableCell align="center" style={{fontWeight:"700"}}>Contact</TableCell>
-                            <TableCell align="center" style={{fontWeight:"700"}}>Email</TableCell>
-                            <TableCell align="center" style={{fontWeight:"700"}}>Check In Date</TableCell>
-                            <TableCell align="center" style={{fontWeight:"700"}}>Check Out Date</TableCell>
-                            <TableCell align="center" style={{fontWeight:"700"}}>Night Count</TableCell>
-                            <TableCell align="center" style={{fontWeight:"700"}}>Room Count</TableCell>
-                            <TableCell align="center" style={{fontWeight:"700"}}>Total Price</TableCell>
-                            <TableCell align="center" style={{fontWeight:"700"}}>Adult Count</TableCell>
-                            <TableCell align="right" style={{fontWeight:"700"}}>Child Count</TableCell>
+                            <TableCell align="left" style={{fontWeight:"700"}}><b>Reservee Name</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Contact</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Email</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Check In Date</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Check Out Date</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Night Count</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Room Count</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Total Price(LKR)</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Adult Count</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Child Count</b></TableCell>
+                            <TableCell align="center" style={{fontWeight:"700"}}><b>Status</b></TableCell>
                             <TableCell></TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
-                        {/* {
-                            reservation.map((reservation) => ( */}
-                                <TableRow>
+                        {
+                            reservations.map((reservation, index) => (
+                                reservation.userId === loggedUserId && (
+                                    <>
+                                <TableRow key={index}>
                                     <TableCell align="left">{reservation.hotelName}</TableCell>
                                     <TableCell align="center">{reservation.reserveeName}</TableCell>
                                     <TableCell align="center">{reservation.contact}</TableCell>
@@ -136,9 +161,10 @@ const MyReservations = () => {
                                     <TableCell align="center">{reservation.checkOutDate}</TableCell>
                                     <TableCell align="center">{reservation.nightCount}</TableCell>
                                     <TableCell align="center">{reservation.roomCount}</TableCell>
-                                    <TableCell align="center">{reservation.totalPrice}</TableCell>
+                                    <TableCell align="center">{reservation.totalPrice}.00</TableCell>
                                     <TableCell align="center">{reservation.adultCount}</TableCell>
                                     <TableCell align="center">{reservation.childCount}</TableCell>
+                                    <TableCell align="center" style={{color:"red"}}>{reservation.status}</TableCell>
                                     <TableCell align="right">
                                         <Stack direction="row" spacing={1}>
                                             <IconButton aria-label="delete" style={{color:"#FF0000"}} onClick={() => handleDeleteReservation(reservation.id)}>
@@ -153,19 +179,21 @@ const MyReservations = () => {
                                         </Stack>
                                     </TableCell>
                                 </TableRow>
-                            {/* ))
-                        } */}
+                                </>
+                                )
+                             ))
+                        } 
                     </TableBody>
                 </Table>
             </TableContainer>
        </>
        {editOpen && reservation &&
-        <EditMyReservation
-          reservation={reservation}
-          setEditOpen={setEditOpen}
-          handleGetReservations={handleGetReservations}
-        />
-      }
+            <EditMyReservation
+            reservation={reservation}
+            setEditOpen={setEditOpen}
+            handleGetReservations={handleGetReservations}
+            />
+        }
     </div>
   )
 }
