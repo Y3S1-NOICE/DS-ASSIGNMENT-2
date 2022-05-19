0af3f2651cfb58ccc0a2d11bdcbc7b2e68c23d6b
amanthajayathilake <amanthajayathilake@gmail.com>
2022-05-19 05:58:43 +0530

    Reservation changes

--- ReservationService/model/customerReservationModel.js
+++ ReservationService/model/customerReservationModel.js
@@ -4,9 +4,9 @@ import {v4} from 'uuid';
 const customerReservationInfoSchema = new mongoose.Schema({
     id: { type: String, default: v4},
 
-    userId: { type: String, required: true,},
+    userId: { type: String, required: true },
     
-    hotelName: { type: String, required: true},
+    hotelName: { type: String, required: true },
 
     reserveeName: { type: String, required: true },
 
